89d9d08c869a0b751fdbd706fb30d45d02077169
Lincoln Lo <Lolincoln23@gmail.com>
2020-08-01 20:01:43 -0400

    update worker and change port of docker compose

--- worker/index.js
+++ worker/index.js
@@ -1,21 +1,20 @@
-const keys = require('./keys');
-const redis = require('redis');
+const keys = require("./keys");
+const redis = require("redis");
 
 const redisClient = redis.createClient({
-    host: keys.redisHost,
-    port: keys.redisPort,
-    retry_strategy: () => 1000
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
 });
-
 const sub = redisClient.duplicate();
 
-function fib(idx){
-    if (idx < 2) return 1;
-    return fib(idx - 1) + fib(idx - 2)
+function fib(index) {
+  console.log("processing number " + index);
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
 }
 
-sub.on('message', (channel, msg) => {
-    redisClient.hset('values', msg, fib(parseInt(msg)))
-})
-
-sub.subscribe('insert')
+sub.on("message", (channel, message) => {
+  redisClient.hset("values", message, fib(parseInt(message)));
+});
+sub.subscribe("insert");
